f4659d9fa63799f813965c51bcded1c6ceebb295
Fabian Skutta <accounts@fabian.lol>
2022-01-06 23:39:11 +0100

    ✨ add light mode favicon + change title when another tab is opened

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -11,3 +11,32 @@ function sleep(milliseconds) {
       }
     }
   }
+
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
+function get_random (list) {
+  return list[Math.floor((Math.random()*list.length))];
+}
+
+// change title when another tab is opened (inspired by https://bitpunk.de/)
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState === 'hidden') {
+    var randomElement = get_random(['😭 I miss you...','👌 Reingeguckt', '(4) Matches 🔥'])
+    var newtitle = `${randomElement} • fabian.lol`;
+    document.title = newtitle;
+  } else {
+    document.title = 'Fabian • fabian.lol'; 
+  }
+})
