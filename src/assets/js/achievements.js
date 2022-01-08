95a5a12cd1077384d67430ec1ad7c1dad6f790fd
Fabian Skutta <accounts@fabian.lol>
2022-01-08 18:21:45 +0100

    :sparkles: add E-Mail link

--- src/assets/js/achievements.js
+++ src/assets/js/achievements.js
@@ -13,7 +13,7 @@ if (n == 10) {
         <svg class="achievement-icon" fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/></svg>
     </div>
     <h3 class="achievement-title">10 visits</h3>
-    <p class="achievement-description">You're visiting this website for the 10th time.</p>
+    <p class="achievement-description">You're visiting my website for the 10th time.</p>
     </div>`;
     const tl = gsap.timeline({defaults: {duration: 0.75}})
     tl.fromTo('.achievement-container', {y: 50, opacity:0}, {y: 0, opacity:1}, "+=5.00")
@@ -32,7 +32,7 @@ if (n == 1) {
         <svg class="achievement-icon" fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/></svg>
     </div>
     <h3 class="achievement-title">Welcome!</h3>
-    <p class="achievement-description">You're visiting this website for the 1st time.</p>
+    <p class="achievement-description">You're visiting my website for the 1st time.</p>
     </div>`;
     const tl = gsap.timeline({defaults: {duration: 0.75}})
     tl.fromTo('.achievement-container', {y: 50, opacity:0}, {y: 0, opacity:1}, "+=5.00")
