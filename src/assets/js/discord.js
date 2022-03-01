0f15f0ede18c2fe306abcf700cac12f6f03654a0
Fabian Skutta <accounts@fabian.lol>
2022-03-01 23:38:40 +0100

    :alien: discord app now opens without detour

--- src/assets/js/discord.js
+++ src/assets/js/discord.js
@@ -1,5 +1,3 @@
-var x = window.matchMedia("(max-width: 900px)")
-
 function copy2Clipboard(text) {
     ta = document.createElement('textarea');
     ta.value = text;
@@ -9,20 +7,12 @@ function copy2Clipboard(text) {
     document.body.removeChild(ta);
 }
 
-if (x.matches) {
-    var btn = "Open the Discord App";
-    var link = "https://discordapp.page.link/?link=https%3A%2F%2Fitunes.apple.com%2Fus%2Fapp%2Fdiscord-chat-for-games%2Fid985746746%3FattemptId%3Dd2c36e44-44b2-4a9f-bb80-f023a246ffdb&utm_source=download&apn=com.discord&isi=985746746&ibi=com.hammerandchisel.discord&sd=Dein%20Ort%2C%20um%20mit%20deinen%20Communitys%20und%20Freunden%20zu%20reden.&efr=1";
-} else {
-    var btn = "Open Discord in your Browser";
-    var link = "https://discordapp.com/users/628637707366694932";
-}
-
 document.getElementById('discord').addEventListener('click', event => {
     document.getElementById('notification').innerHTML = `
     <div class="notification-container">
     <h3 class="notification-title">Discord</h3>
     <p class="notification-description">My Discord username has been copied to your clipboard. (Fahrbahn#7177)</p>
-    <a href="${link}" target="_blank" class="btn-small btn-primary">${btn}</a>
+    <a href="https://discordapp.com/users/628637707366694932" target="_blank" class="btn-small btn-primary">Open Discord</a>
     <a id="notification-close" class="btn-small btn-secondary">close</a>
     </div>`;
     tl.fromTo('.notification-container', {y: 50, opacity:0}, {y: 0, opacity:1})
